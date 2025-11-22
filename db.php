522feec2250ef0f8f521e91bc80b26ae96e57974
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2025-11-22 13:49:19 +0700

    config db

--- db.php
+++ db.php
@@ -1,11 +1,16 @@
 <?php
 // Cấu hình PDO
-// define('DB_HOST','103.139.203.43:887');
-// define('DB_PORT', 3306);
+// define('DB_HOST','localhost');
 // define('DB_NAME','sql_nhom31_itimi');
-// define('DB_USER','root');
+// define('DB_USER','sql_nhom31_itimi');
 // define('DB_PASS','717a636a41b2e');
 
+// file zilla username = ftp_nhom31_itimit_id_vn
+// pass : 18dc2dbc9b5508
+// host : 103.139.203.43
+// post : 21
+
+// -----------------------------------------------------
 define('DB_HOST','127.0.0.1');
 define('DB_NAME','nhathuocantam');
 define('DB_USER','root');
